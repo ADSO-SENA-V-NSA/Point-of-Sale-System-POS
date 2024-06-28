17eeb1e96f9847b3ca8de9aafbea16a64914db7b
Camilo Arango <ccarango17@soy.sena.edu.co>
2024-06-27 19:08:48 -0500

    Sidebar 80%

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from "./pages/main";
+import Header from "./components/header";
 
 function App() {
   return (
     <Router>
+      <Header/>
       <Routes>
-        <Route path="/" element={<Main/>}/>
+        <Route path="/"/>
       </Routes>
     </Router>
   );
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,23 +2,71 @@ import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
+const NavDiv = styled.div`
+    display: flex;
+    flex-direction: column-reverse;
+`
+const Sidebar = styled.aside`
+    background-color: #15171C;
+    width: 250px;
+    height: 80vh;
+    margin-left: 30px;
+    margin-top: 30px;
+    border-radius: 15px;
+    outline: 1px solid #1d2027;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+const CatLogo = styled.img`
+    margin-top: 20px;
+    width: 80px;
+    text-align: center;
+`
+const List = styled.ul`
+    list-style: none;
+    margin-top: 50px;
+
+`
+const ListLink = styled(Link)`
+    text-decoration: none;
+    position: relative;
+    right: 60px;
+    gap: 20px;
+    color: #626573;
+    font-weight: bold;
+    font-size: 1.5rem;
+    transition: 200ms ease-in-out;
+    &:hover{
+        color: #006AE6;
+    }
+`
+const ListItem = styled.li`
+    margin-bottom: 20px;
+`
+const Navbar = styled.header`
+    width: 100%;
+    height: 80px;
+    box-shadow: 0 2px 0 #1d2027;
+`
+
 export default function Header (){
     return(
-        <div>
-            <aside>
-                <img src="" alt="Logo" />
-                <ul>
-                    <li><Link to={"/"}>Dashboard</Link></li>
-                    <li><Link to={"/caja"}>Caja</Link></li>
-                    <li><Link to={"/productos"}>Productos</Link></li>
-                    <li><Link to={"/facturas"}>Facturas</Link></li>
-                </ul>
-            </aside>
-            <header>
+        <NavDiv>
+            <Sidebar>
+                <CatLogo src="images/cat_logo.svg" alt="Logo" />
+                <List>
+                    <ListItem><ListLink to={"/"}>Dashboard</ListLink></ListItem>
+                    <ListItem><ListLink to={"/caja"}>Caja</ListLink></ListItem>
+                    <ListItem><ListLink to={"/productos"}>Productos</ListLink></ListItem>
+                    <ListItem><ListLink to={"/facturas"}>Facturas</ListLink></ListItem>
+                </List>
+            </Sidebar>
+            <Navbar>
                 <div>
                     <h1>Hola Dashboard</h1>
                 </div>
-            </header>
-        </div>
+            </Navbar>
+        </NavDiv>
     );
 };
